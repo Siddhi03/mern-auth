d225511eea35e1567430761041a553075876ef36
Siddhi Jain <jainsiddhi03@gmail.com>
2024-04-14 22:26:43 +0530

    Added api (backend) folder and created routes and controllers in api . Created signup page UI

--- api/index.js
+++ api/index.js
@@ -1,14 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
-dotenv.config();
+import userRoutes from "./routes/user.route.js";
+import authRoutes from  "./routes/auth.router.js";
 
+dotenv.config();
 const app = express();
+app.use(express.json());
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to db")
 }).catch((err)=>{
     console.log(err);
 })
+
+app.use("/api/user", userRoutes)
+app.use("/api/auth",authRoutes)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error"
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    })
+    
+});
 
 app.listen(3000, ()=>{
     console.log("Server listening at 3000")
